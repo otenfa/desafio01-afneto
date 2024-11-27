87d7f00c3566d92cfaa792a917c501214af9ece8
Aristóteles Ferrão <afneto1960@gmail.com>
2024-11-27 17:27:04 -0300

    criação projeto maven, ajustes no arquivo pom.xml, teste de execução ṕós importação c/ texto no log do STS

---
+++ src/main/java/com/afneto/desafio01/Desafio01Application.java
@@ -0,0 +1,21 @@
+package com.afneto.desafio01;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio01Application implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio01Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		System.out.println("Desafio 01 em execução!");
+		
+	}
+
+}
